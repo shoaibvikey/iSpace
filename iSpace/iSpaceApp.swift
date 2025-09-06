323b31717a1062b7881a5e8b2e8d041598751274
shoaibvikey <shoaibakhtarvikey@gmail.com>
2025-09-06 15:32:49 +0530

    Initial Commit

---
+++ iSpace/iSpaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  iSpaceApp.swift
+//  iSpace
+//
+//  Created by Shoaib Akhtar on 06/09/25.
+//
+
+import SwiftUI
+
+@main
+struct iSpaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ iSpaceTests/iSpaceTests.swift
@@ -0,0 +1,17 @@
+//
+//  iSpaceTests.swift
+//  iSpaceTests
+//
+//  Created by Shoaib Akhtar on 06/09/25.
+//
+
+import Testing
+@testable import iSpace
+
+struct iSpaceTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
